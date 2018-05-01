eebd3e97270adf8491eb0add00ddee9554d2d7ab
kevin wilkinson <willkillson@gmail.com>
2018-04-30 17:32:16 -0700

    true/false sequence programmed.

--- Challenge-359-Easy-JS-/js/main.js
+++ Challenge-359-Easy-JS-/js/main.js
@@ -1,1 +1,68 @@
-﻿// Your code here!
+﻿
+
+let display = document.getElementById('outputSpot');
+let display_n = document.getElementById('Ncount')
+
+let sequencer = new Sequencer();
+
+function Sequencer() {
+    this.sequence = null;
+    this.currentNumber = 0;
+    this.sequencebase10 = null;
+
+    this.generateNew = function () {
+
+        if (this.sequence === null) {
+            this.sequence = new Array();
+            this.sequence.push(true);
+        }
+        else {
+            let old = this.sequence;
+          
+            let addthis = this.sequencesComplement(old);
+            this.sequence.push(true);
+            for (let i = 0; i < addthis.length; i++) {
+                this.sequence.push(addthis[i]);
+            }
+
+        }
+
+        this.sequencebase10 = converttobase10();
+        this.currentNumber++;
+    }
+    this.sequencesComplement = function (old) {
+        let rhsArray = new Array(old.length);
+
+        for (let i = 0; i < old.length; i++) {
+            if (old[i] === true) {
+                rhsArray[i] = false;
+            }
+            else {
+                rhsArray[i] = true;
+            }
+        }
+        rhsArray.reverse();
+        return rhsArray;
+    }
+    this.converttobase10 = function () {
+
+
+    }
+   
+
+}
+
+function process() {
+
+    sequencer.generateNew();
+
+}
+
+processBTN = document.getElementById('output');
+processBTN.onclick = function () {
+
+    process();
+    display_n.innerText = sequencer.currentNumber;
+    display.innerText = sequencer.sequence;
+}
+
